a818267753bc2b2f3aa78624d2298fd24257ea29
Gabriele Restuccia <gabriele.restuccia92@gmail.com>
2022-03-03 11:59:43 +0100

    node values filtering improved

--- py_epanet2csv.py
+++ py_epanet2csv.py
@@ -13,22 +13,44 @@ def toCSV():
     if INPUT_FILE_PATH == "":
         sys.exit("Input file path cannot be empty")
 
-    with open(INPUT_FILE_PATH,'r',encoding = 'utf-8') as inputFile: # 'with' closes the file automatically
+    f = open(INPUT_FILE_PATH, "r")
 
-        for line in inputFile: # read each line of the file
-            if MATCH in line: # found a table
-                found = True
-                table_count += 1
+    line = f.readline()
+
+    while line:
+        if MATCH in line:
+            found = True
+            table_count += 1
+            splitted = line.split( )
+            hour = splitted[3] # store the timestamp
+
+            line = f.readline() # need to skip table headers (5 lines)
+            line = f.readline()
+            line = f.readline()
+            line = f.readline()
+            line = f.readline()
+
+            splitted = line.split( )
+
+            while (len(splitted) > 0 and splitted[0].isdigit()):
+                nodeID = splitted[0] # store the timestamp
+                demand = splitted[1]
+                head = splitted[2]
+                pressure = splitted[3]
+                chlorine = splitted[4]
+                print(hour,nodeID,demand,head,pressure,chlorine)
+
+                line = f.readline()
                 splitted = line.split( )
 
-                hour = splitted[3] # store the timestamp
+        line = f.readline()
 
-                print("Found!",hour)
+    f.close()
 
-        if found:
-            print("Found %d tables" % table_count)
-        else:
-            print("Cannot find any Node tables inside input file!")
+    if found:
+        print("Found %d tables" % table_count)
+    else:
+        print("Cannot find any Node tables inside input file!")
 
 if __name__ == "__main__":
 # Program description
