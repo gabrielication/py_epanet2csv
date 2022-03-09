033abad1f0592240041c2f626ea70a840f18999e
Gabriele Restuccia <gabriele.restuccia92@gmail.com>
2022-03-09 16:51:38 +0100

    double plot added

--- csv_to_plot.py
+++ csv_to_plot.py
@@ -1,54 +1,167 @@
+import argparse
 import csv
 import matplotlib.pyplot as plt
+import numpy as np
 
-timestamp_axis = []
-node_demand_axis = []
-reservoir_demand_axis = {} #we need a dictionary of reservoir demand! we have multiple single values for the same timestamp
+filename_input = ""
 
-with open('output.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
+def plot_node_and_reservoir_demand(filename):
+    timestamp_axis = []
+    node_demand_axis = []
+    reservoir_demand_axis = {}  # we need a dictionary of reservoir demand! we have multiple single values for the same timestamp
 
-    node_demand_sum = 0.0
+    with open(filename) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
 
-    for line in csv_reader:
-        if len(timestamp_axis) == 0: #if this is the first row that we are reading then we can insert the first timestamp
-            timestamp_axis.append(line[0]) 
-        elif (line[0] != timestamp_axis[-1]): #if this is the next timestamp that we are reading then we have to append the new one and set the other values
-            timestamp_axis.append(line[0]) #insert new timestamp
-            node_demand_axis.append(node_demand_sum) #we have to save the sum of junctions otherwise it will be lost
-            #print(node_demand_sum)
-            node_demand_sum = 0.0 #we can reset the sum
+        node_demand_sum = 0.0
+        total_sum = 0.0
 
-        demand = float(line[2])
+        for line in csv_reader:
+            if len(timestamp_axis) == 0:
+                # if this is the first row that we are reading then we can insert the first timestamp
+                timestamp_axis.append(line[0])
+            elif (line[0] != timestamp_axis[-1]):
+                # if this is the next timestamp that we are reading then we have to append the new one and set the other values
+                # insert new timestamp
+                timestamp_axis.append(line[0])
+                # we have to save the sum of junctions otherwise it will be lost
+                node_demand_axis.append(node_demand_sum)
+                # we can reset the sum
+                #print(total_sum)
+                node_demand_sum = 0.0
 
-        if(line[-1] == "Junction"):
-            node_demand_sum += demand
-        elif(line[-1] == "Reservoir"):
-            reservoir_id = line[1]
+            demand = float(line[2])
 
-            #demand = abs(demand)
+            total_sum += demand
 
-            if(reservoir_id in reservoir_demand_axis.keys()):
-                reservoir_demand_axis[reservoir_id].append(demand)
-            else:
-                reservoir_demand_axis[reservoir_id] = [demand]
-            
+            if (line[-1] == "Junction"):
+                node_demand_sum += demand
+            elif (line[-1] == "Reservoir"):
+                reservoir_id = line[1]
 
-    node_demand_axis.append(node_demand_sum) #if we dont do this we lose the last sum
+                # demand = abs(demand)
 
-# plotting the points
-plt.plot(timestamp_axis, node_demand_axis, label="Junctions Demand")
+                if (reservoir_id in reservoir_demand_axis.keys()):
+                    reservoir_demand_axis[reservoir_id].append(demand)
+                else:
+                    reservoir_demand_axis[reservoir_id] = [demand]
 
-for key in reservoir_demand_axis:
-    plt.plot(timestamp_axis, reservoir_demand_axis[key], label = key)
- 
-# naming the x axis
-plt.xlabel('Timestamp (hour)')
-# naming the y axis
-plt.ylabel('Demand (GPM)')
- 
-# giving a title to my graph
-plt.title('Demand Graph')
- 
-# function to show the plot
-plt.show()
+        node_demand_axis.append(node_demand_sum)  # if we dont do this we lose the last sum
+
+    # plotting the points
+    plt.plot(timestamp_axis, node_demand_axis, label="Junctions Demand")
+
+    for key in reservoir_demand_axis:
+        plt.plot(timestamp_axis, reservoir_demand_axis[key], label=key)
+
+    # naming the x axis
+    plt.xlabel('Timestamp (hour)')
+    # naming the y axis
+    plt.ylabel('Demand (GPM)')
+
+    # giving a title to my graph
+    plt.title('Demand Graph')
+
+    # function to show the plot
+    plt.show()
+
+def plot_demand_and_pressure(filename):
+    node1 = "8600"
+    node2 = "8742"
+
+    timestamp_axis = []
+    node1_demand_axis = []
+    node2_demand_axis = []
+
+    node1_pressure_axis = []
+    node2_pressure_axis = []
+
+    reservoir_demand_axis = {}  # we need a dictionary of reservoir demand! we have multiple single values for the same timestamp
+    reservoir_pressure_axis = {}  # we need a dictionary of reservoir demand! we have multiple single values for the same timestamp
+
+    with open(filename) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+
+        for line in csv_reader:
+            if len(timestamp_axis) == 0:
+                # if this is the first row that we are reading then we can insert the first timestamp
+                timestamp_axis.append(line[0])
+            elif (line[0] != timestamp_axis[-1]):
+                timestamp_axis.append(line[0])
+
+            demand = float(line[2])
+            pressure = float(line[4])
+
+            if (line[-1] == "Junction"):
+                node_id = line[1]
+
+                if (node_id == node1):
+                    node1_demand_axis.append(demand)
+                    node1_pressure_axis.append(pressure)
+                elif (node_id == node2):
+                    node2_demand_axis.append(demand)
+                    node2_pressure_axis.append(pressure)
+
+            elif (line[-1] == "Reservoir"):
+                reservoir_id = line[1]
+
+                # demand = abs(demand)
+
+                if (reservoir_id in reservoir_demand_axis.keys()):
+                    reservoir_demand_axis[reservoir_id].append(demand)
+                else:
+                    reservoir_demand_axis[reservoir_id] = [demand]
+
+                if (reservoir_id in reservoir_pressure_axis.keys()):
+                    reservoir_pressure_axis[reservoir_id].append(pressure)
+                else:
+                    reservoir_pressure_axis[reservoir_id] = [pressure]
+
+    #for key in reservoir_demand_axis:
+    #    plt.plot(timestamp_axis, reservoir_demand_axis[key], label=key)
+
+    fig, axs = plt.subplots(2)
+    #fig.suptitle('Demand and Pressure subplots')
+
+    axs[0].plot(timestamp_axis, node1_demand_axis)
+    axs[0].plot(timestamp_axis, node2_demand_axis)
+    axs[0].set_title("Demand for Junction "+node1+" and "+node2+" and reservoirs")
+
+    #for key in reservoir_demand_axis:
+    #    axs[0].plot(timestamp_axis, reservoir_demand_axis[key])
+
+    axs[1].plot(timestamp_axis, node1_pressure_axis)
+    axs[1].plot(timestamp_axis, node2_pressure_axis)
+    axs[1].set_title("Pressure for Junction " + node1 + " and " + node2)
+
+    # function to show the plot
+    plt.show()
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    # Program description
+    text = "This script processes the report file from EPANET 2.2, extracts Node Results and converts them to csv. " \
+           "Please type the path of the file you want to convert. "
+
+    # Initiate the parser with a description
+    parser = argparse.ArgumentParser(description=text)
+
+    # Add long and short argument
+    parser.add_argument("--input", "-i", help="Input file for processing")
+    parser.add_argument("--mode", "-m", help="Mode to choose: 'nd': Plot Junction and Reservoir demands, 'dp': Plot Demand and Pressure")
+
+    # Read arguments from the command line
+    args = parser.parse_args()
+
+    # Check for --name
+    if args.input and args.mode:
+        filename_input = args.input
+        if args.mode == "nd":
+            plot_node_and_reservoir_demand(filename_input)
+        elif args.mode == "dp":
+            plot_demand_and_pressure(filename_input)
+        else:
+            print("Mode not supported. See 'csv_to_plot.py -h'")
+    else:
+        print("Usage 'csv_to_plot.py -i inputfile -m mode'")
--- py_epanet2csv.py
+++ py_epanet2csv.py
@@ -1,15 +1,15 @@
 import sys
 import argparse
-import re
 import csv
 
-MATCH = "Node Results" #We use this string as a filter to find the tables for Node Values
+MATCH = "Node Results"  # We use this string as a filter to find the tables for Node Values
 INPUT_FILE_PATH = ""
 
-def toCSV():
+
+def to_csv():
     found = False
 
-    table_count = 0 #how many tables we found?
+    table_count = 0  # how many tables we found?
 
     if INPUT_FILE_PATH == "":
         sys.exit("Input file path cannot be empty")
@@ -17,7 +17,7 @@ def toCSV():
     f = open(INPUT_FILE_PATH, "r")
 
     outName = "output.csv"
-    out = open(outName,"w")
+    out = open(outName, "w")
     writer = csv.writer(out)
 
     line = f.readline()
@@ -26,37 +26,35 @@ def toCSV():
         if MATCH in line:
             found = True
             table_count += 1
-            splitted = line.split( )
-            hour = splitted[3] # store the timestamp
+            splitted = line.split()
+            hour = splitted[3]  # store the timestamp
 
-            line = f.readline() # need to skip table headers (5 lines)
+            line = f.readline()  # need to skip table headers (5 lines)
             line = f.readline()
             line = f.readline()
             line = f.readline()
             line = f.readline()
 
-            splitted = line.split( )
-
-            
+            splitted = line.split()
 
             while (len(splitted) > 0):
                 nodeID = splitted[0]
                 demand = splitted[1]
                 head = splitted[2]
                 pressure = splitted[3]
-                #chlorine = splitted[4]
+                # chlorine = splitted[4]
 
-                splitted.insert(0,hour)
+                splitted.insert(0, hour)
 
-                if(len(splitted) == 5):
+                if (len(splitted) == 5):
                     splitted.append("Junction")
 
-                #print(splitted)
+                # print(splitted)
 
                 writer.writerow(splitted)
 
                 line = f.readline()
-                splitted = line.split( )
+                splitted = line.split()
 
         line = f.readline()
 
@@ -68,12 +66,14 @@ def toCSV():
     else:
         print("Cannot find any Node tables inside input file!")
 
+
 if __name__ == "__main__":
-# Program description
-    text =  "This script processes the report file from EPANET 2.2, extracts Node Results and converts them to csv. Please type the path of the file you want to convert."
+    # Program description
+    text = "This script processes the report file from EPANET 2.2, extracts Node Results and converts them to csv. " \
+           "Please type the path of the file you want to convert. "
 
     # Initiate the parser with a description
-    parser = argparse.ArgumentParser(description = text)
+    parser = argparse.ArgumentParser(description=text)
 
     # Add long and short argument
     parser.add_argument("--input", "-i", help="Input file for processing")
@@ -84,6 +84,6 @@ if __name__ == "__main__":
     # Check for --name
     if args.input:
         INPUT_FILE_PATH = args.input
-        toCSV()
+        to_csv()
     else:
         print("Usage 'py_epanet2csv.py -i inputfile'")
