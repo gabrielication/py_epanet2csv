5f1003d9b5c08c755c45c469d1c901d0c44a9844
Gabriele Restuccia <gabriele.restuccia92@gmail.com>
2022-03-22 14:01:34 +0100

    map extractor refactor

--- epanet_map_extractor.py
+++ epanet_map_extractor.py
@@ -1,16 +1,9 @@
 import csv
 
-def extract_nodes(input_file, up_x_coord, up_y_coord, down_x_coord, down_y_coord):
+def extract_nodes(input_file, min_x_coord, max_y_coord, max_x_coord, min_y_coord, selected_ids=[]):
     outName = "extracted_nodes.csv"
     out = open(outName, "w")
     writer = csv.writer(out)
-
-    #494155.350
-    #1379384.780
-
-    #1376590.440
-
-    selected_ids = ["8596"]
 
     with open(input_file) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
@@ -20,13 +13,13 @@ def extract_nodes(input_file, up_x_coord, up_y_coord, down_x_coord, down_y_coord
             node_x = float(line[5])
             node_y = float(line[6])
 
-            if(node_id in selected_ids):
+            if(len(selected_ids) > 0 and node_id in selected_ids):
                 writer.writerow(line)
 
-            elif(node_x >= up_x_coord):
-                if(node_x <= down_x_coord):
-                    if(node_y <= up_y_coord):
-                        if(node_y >= down_y_coord):
+            elif(node_x >= min_x_coord):
+                if(node_x <= max_x_coord):
+                    if(node_y <= max_y_coord):
+                        if(node_y >= min_y_coord):
                             writer.writerow(line)
 
     out.close()
@@ -34,4 +27,4 @@ def extract_nodes(input_file, up_x_coord, up_y_coord, down_x_coord, down_y_coord
     print("Extracted nodes to "+outName)
 
 if __name__ == "__main__":
-    extract_nodes("output.csv",494318.100,1379694.190,500947.500,1376590.440)
+    extract_nodes("output.csv",494318.100,1379694.190,500947.500,1376590.440,["8596"])
