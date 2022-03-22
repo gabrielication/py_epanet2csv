960b13fa004c9ac9f5a5fa6eb9236149ce1fe127
Gabriele Restuccia <gabriele.restuccia92@gmail.com>
2022-03-22 13:55:21 +0100

    fix for map extractor coords

--- epanet_map_extractor.py
+++ epanet_map_extractor.py
@@ -24,13 +24,14 @@ def extract_nodes(input_file, up_x_coord, up_y_coord, down_x_coord, down_y_coord
                 writer.writerow(line)
 
             elif(node_x >= up_x_coord):
-                if(node_y <= up_y_coord):
-                    if(node_y >= down_y_coord):
-                        writer.writerow(line)
+                if(node_x <= down_x_coord):
+                    if(node_y <= up_y_coord):
+                        if(node_y >= down_y_coord):
+                            writer.writerow(line)
 
     out.close()
 
     print("Extracted nodes to "+outName)
 
 if __name__ == "__main__":
-    extract_nodes("output.csv",494318.100,1379694.190,0,1376590.440)
+    extract_nodes("output.csv",494318.100,1379694.190,500947.500,1376590.440)
