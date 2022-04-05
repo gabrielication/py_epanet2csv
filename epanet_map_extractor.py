06fe6a5034bad59731b025e743f307c93007ddfb
Gabriele Restuccia <gabriele.restuccia92@gmail.com>
2022-04-05 14:51:36 +0200

    map extractor now supports links too

--- epanet_map_extractor.py
+++ epanet_map_extractor.py
@@ -2,10 +2,36 @@ import csv
 import sys
 import argparse
 
+def extract_links(input_file, extracted_nodes):
+    outName = "extracted_links.csv"
+    out = open(outName, "w")
+    writer = csv.writer(out)
+
+    extracted_links = []
+
+    with open(input_file) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+
+        for line in csv_reader:
+            link_id = line[1]
+            start_node = line[-3]
+            end_node = line[-2]
+
+            if((start_node in extracted_nodes) or (end_node in extracted_nodes)):
+                writer.writerow(line)
+                extracted_links.append(link_id)
+    out.close()
+
+    print("Extracted links to " + outName)
+
+    return extracted_links
+
 def extract_nodes(input_file, min_x_coord, max_y_coord, max_x_coord, min_y_coord, selected_ids=[]):
     outName = "extracted_nodes.csv"
     out = open(outName, "w")
     writer = csv.writer(out)
+
+    extracted_nodes = []
 
     with open(input_file) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
@@ -17,16 +43,20 @@ def extract_nodes(input_file, min_x_coord, max_y_coord, max_x_coord, min_y_coord
 
             if(len(selected_ids) > 0 and node_id in selected_ids):
                 writer.writerow(line)
+                extracted_nodes.append(node_id)
 
             elif(node_x >= min_x_coord):
                 if(node_x <= max_x_coord):
                     if(node_y <= max_y_coord):
                         if(node_y >= min_y_coord):
                             writer.writerow(line)
+                            extracted_nodes.append(node_id)
 
     out.close()
 
     print("Extracted nodes to "+outName)
+
+    return extracted_nodes
 
 if __name__ == "__main__":
     # Program description
@@ -47,6 +77,10 @@ if __name__ == "__main__":
     # Read arguments from the command line
     args = parser.parse_args()
 
+    extracted_nodes = extract_nodes("nodes_output.csv", 494318.100, 1379694.190, 500947.500, 1376590.440, ["8596"])
+    extracted_links = extract_links("links_output.csv", extracted_nodes)
+
+"""
     if args.input and args.Xmin and args.Xmax and args.Ymin and args.Ymax:
         input_file = args.input
         x_min = float(args.Xmin)
@@ -61,7 +95,6 @@ if __name__ == "__main__":
             extract_nodes(input_file, x_min, y_max, x_max, y_min, [])
     else:
         print("Use 'epanet_map_extractor.py -h'")
-
-    #extract_nodes("junctions_output.csv",494318.100,1379694.190,500947.500,1376590.440,["8596"])
+"""
 
     #python3 epanet_map_extractor.py -i junctions_output.csv -xmin 494318.100 -ymax 1379694.190 -xmax 500947.500 -ymin 1376590.440 -n 8596
