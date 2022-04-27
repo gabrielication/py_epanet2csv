876f31f96942c07571a1cb885ef60cc5ba0e1dae
Gabriele Restuccia <gabriele.restuccia92@gmail.com>
2022-04-27 10:39:35 +0200

    code usability improved

--- py_epanet2csv.py
+++ py_epanet2csv.py
@@ -1,45 +1,75 @@
+import argparse
+
 import wntr
 import pandas as pd
 import csv
 
-inp_file = 'networks/Net3.inp'
+def run_nodes_sim_to_csv(inp_file):
+    print("Running simulation...")
 
-wn = wntr.network.WaterNetworkModel(inp_file)
+    wn = wntr.network.WaterNetworkModel(inp_file)
 
-node_names = wn.node_name_list
+    node_names = wn.node_name_list
 
-results = wntr.sim.EpanetSimulator(wn).run_sim()
+    results = wntr.sim.EpanetSimulator(wn).run_sim()
 
-demand_results= results.node['demand']
-head_results= results.node['head']
-pressure_results= results.node['pressure']
+    print("Simulation finished. Writing to csv (can take a while)...")
 
-indexes = demand_results.index
+    demand_results = results.node['demand']
+    head_results = results.node['head']
+    pressure_results = results.node['pressure']
 
-outName = "nodes_output.csv"
-out = open(outName, "w")
-writer = csv.writer(out)
+    indexes = demand_results.index
 
-for timestamp in indexes:
-    tot_demand = 0.0
+    outName = "nodes_output.csv"
+    out = open(outName, "w")
+    writer = csv.writer(out)
 
-    for nodeID in node_names:
-        node_obj = wn.get_node(nodeID)
-
-        demand_value = demand_results.loc[timestamp, nodeID]
-        head_value = head_results.loc[timestamp, nodeID]
-        pressure_value = pressure_results.loc[timestamp, nodeID]
-
+    for timestamp in indexes:
+        tot_demand = 0.0
         hour = pd.to_datetime(timestamp, unit='s').time()
-        tot_demand += demand_value
 
-        x_pos = node_obj.coordinates[0]
-        y_pos = node_obj.coordinates[1]
+        for nodeID in node_names:
+            node_obj = wn.get_node(nodeID)
 
-        node_type = node_obj.__class__.__name__
+            demand_value = demand_results.loc[timestamp, nodeID]
+            head_value = head_results.loc[timestamp, nodeID]
+            pressure_value = pressure_results.loc[timestamp, nodeID]
 
-        output_row = [hour, nodeID, demand_value, head_value, pressure_value, x_pos, y_pos, node_type]
+            tot_demand += demand_value
 
-        writer.writerow(output_row)
+            x_pos = node_obj.coordinates[0]
+            y_pos = node_obj.coordinates[1]
 
-out.close()
+            node_type = node_obj.__class__.__name__
+
+            output_row = [hour, nodeID, demand_value, head_value, pressure_value, x_pos, y_pos, node_type]
+
+            writer.writerow(output_row)
+
+        print("Tot demand at "+str(hour)+" is: "+str(tot_demand))
+
+    out.close()
+
+    print("Finished!")
+
+if __name__ == "__main__":
+    # Program description
+    text = "This program processes the report file from EPANET 2.2, extracts Node Results and converts them to csv. " \
+           "Please type the path of the file you want to convert. "
+
+    # Initiate the parser with a description
+    parser = argparse.ArgumentParser(description=text)
+
+    # Add long and short argument
+    parser.add_argument("--input", "-i", help="Input file for processing")
+
+    # Read arguments from the command line
+    args = parser.parse_args()
+
+    # Check for --name
+    if args.input:
+        input_file = args.input
+        run_nodes_sim_to_csv(input_file)
+    else:
+        print("See 'py_epanet2csv.py -h' in order to know how to correctly execute this program")
