ded48270d9ae4ee63e417d3459ee603ef923c968
Gabriele Restuccia <gabriele.restuccia92@gmail.com>
2022-05-19 12:06:36 +0200

    switched to a classifier model, working well

--- ml_analysis.py
+++ ml_analysis.py
@@ -1,11 +1,16 @@
 
 from sklearn import linear_model
+from sklearn.metrics import accuracy_score
 from sklearn.metrics import mean_squared_error, r2_score
+
+from sklearn import svm
+
+import numpy as np
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-filename = 'nodes_output(1).csv'
+filename = 'nodes_output(3).csv'
 
 print("Loading csv...")
 
@@ -18,11 +23,11 @@ attributes_X = data[["demand_value","head_value","pressure_value","current_leak_
 
 print("Dividing X and y matrices...")
 
-attributes_X = data[["head_value","pressure_value"]].copy()
-target_y = data["has_leak"].copy()
-#target_y = data[["has_leak","leak_area_value","leak_discharge_value","current_leak_demand_value"]].copy()
+attributes_X = data[["demand_value","head_value","pressure_value"]].copy()
+target_y = data["has_leak"].astype(int)
 
-attributes_X["tot_demand"] = data["demand_value"] + data["current_leak_demand_value"]
+#target_y = data[["has_leak","leak_area_value","leak_discharge_value","current_leak_demand_value"]].copy()
+#attributes_X["tot_demand"] = data["demand_value"] + data["current_leak_demand_value"]
 
 X_train, X_test, y_train, y_test = train_test_split(attributes_X, target_y,test_size=0.2)
 
@@ -31,8 +36,9 @@ X_test.reset_index(drop=True, inplace=True)
 y_train.reset_index(drop=True, inplace=True)
 y_test.reset_index(drop=True, inplace=True)
 
+print(np.unique(y_train))
 # Create linear regression object
-regr = linear_model.LinearRegression()
+regr = svm.SVC(kernel="linear")
 
 print("Training started...")
 # Train the model using the training sets
@@ -46,8 +52,9 @@ print("Prediction started...")
 target_y_pred1 = regr.predict(X_train)
 target_y_pred2 = regr.predict(X_test)
 
-print("Coefficient of determination: %.2f" % r2_score(y_train, target_y_pred1))
+#print("Coefficient of determination: %.2f" % r2_score(y_train, target_y_pred1))
 print("Coefficient of determination: %.2f" % r2_score(y_test, target_y_pred2))
+print("Accuracy score is: "+str(accuracy_score(y_test, target_y_pred2)))
 
 #print("R2 Score on X_test and y_test is: "+str(r2_score(X_test, y_test)))
 
