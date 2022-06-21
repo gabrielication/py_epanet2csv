06f713c53e377e0a85445bc470099dc955bfa4d6
Gabriele Restuccia <gabriele.restuccia92@gmail.com>
2022-06-21 15:27:52 +0200

    classifier comparison now outputs more values and evaluates Ensemble classifier

--- plot_classifier_comparison.py
+++ plot_classifier_comparison.py
@@ -9,6 +9,8 @@ from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import confusion_matrix, accuracy_score
 
+from mlxtend.classifier import EnsembleVoteClassifier
+
 import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -23,19 +25,10 @@ names = [
     "RandomForest",
     "AdaBoostClassifier",
     "GaussianNaiveBayes",
+    "Ensemble",
 ]
 
-classifiers = [
-    KNeighborsClassifier(),
-    SVC(kernel="linear"),
-    SVC(kernel="rbf",gamma='auto'),
-    DecisionTreeClassifier(),
-    RandomForestClassifier(),
-    AdaBoostClassifier(),
-    GaussianNB(),
-]
-
-def execute_classifier(model, name, k_folds, X, y):
+def execute_classifier(model, name, k_folds, X, y, prefix_output_filename):
     print("Executing "+name+"...")
 
     prediction_accuracy = 0.0
@@ -65,6 +58,16 @@ def execute_classifier(model, name, k_folds, X, y):
     group_counts = ["{0:0.0f}".format(value) for value in
                     mean_of_conf_matrix_arrays.flatten()]
 
+    true_negatives = float(group_counts[0])
+    false_positives = float(group_counts[1])
+    false_negatives = float(group_counts[2])
+    true_positives = float(group_counts[3])
+
+    calc_accuracy = (true_positives + true_negatives) / (true_positives + true_negatives + false_negatives + false_positives)
+    precision = true_positives / (true_positives + false_positives)
+    recall = true_positives / (true_positives + false_negatives)
+    false_positive_rate = false_positives / (false_positives + true_negatives)
+
     group_percentages = ["{0:.2%}".format(value) for value in
                          mean_of_conf_matrix_arrays.flatten() / np.sum(mean_of_conf_matrix_arrays)]
 
@@ -84,39 +87,67 @@ def execute_classifier(model, name, k_folds, X, y):
     ax.yaxis.set_ticklabels(['False', 'True'])
 
     ## Display the visualization of the Confusion Matrix.
-    filename = name+'.png'
+    filename = prefix_output_filename+name+'.png'
     plt.savefig(filename)
     plt.clf()
-    return prediction_accuracy
+    return prediction_accuracy, calc_accuracy, precision, recall, false_positive_rate
 
+def execute_classifier_comparison(input_filename, prefix_output_filename):
+    print("Loading "+input_filename+"...")
 
-if __name__ == "__main__":
-    print("Plot classifier comparison started!\n")
-    filename = 'nodes_output.csv'
-    print("Loading csv...")
+    data = pd.read_csv(input_filename,
+                       names=["hour", "nodeID", "demand_value", "head_value", "pressure_value", "x_pos", "y_pos",
+                              "node_type", "has_leak", "leak_area_value", "leak_discharge_value",
+                              "current_leak_demand_value"])
 
-    data = pd.read_csv(filename, names=["hour","nodeID","demand_value","head_value","pressure_value","x_pos", "y_pos",
-                                        "node_type", "has_leak", "leak_area_value", "leak_discharge_value", "current_leak_demand_value"])
+    output_filename = prefix_output_filename+'prediction_accuracies.csv'
 
     # open the file in the write mode
-    f = open('prediction_accuracies.csv', 'w')
+    f = open(output_filename, 'w')
 
     # create the csv writer
     writer = csv.writer(f)
 
+    header = ["Classificator","Accuracy","Calc Accuracy", "Precision", "Recall", "False Positive Rate"]
+    writer.writerow(header)
+
     print("Dividing X and y matrices...\n")
 
-    X = data[["demand_value","head_value","pressure_value"]].copy()
+    X = data[["demand_value", "head_value", "pressure_value"]].copy()
     y = data["has_leak"].astype(int)
 
-    for name, clf in zip(names, classifiers):
-        prediction_accuracy = execute_classifier(clf,name, 5, X,y)
-        print(name+"'s prediction accuracy (mean of kfolds) is: "+str(prediction_accuracy)+"\n")
+    clf1 = KNeighborsClassifier()
+    clf2 = SVC(kernel="linear")
+    clf3 = SVC(kernel="rbf",gamma='auto')
+    clf4 = DecisionTreeClassifier()
+    clf5 = RandomForestClassifier()
+    clf6 = AdaBoostClassifier()
+    clf7 = GaussianNB()
 
-        output_row = [name,prediction_accuracy]
+    eclf = EnsembleVoteClassifier(clfs=[clf1, clf2, clf3, clf4, clf5, clf6, clf7], weights=[1, 1, 1, 1, 1, 1, 1], voting="hard")
+
+    classifiers = [clf1, clf2, clf3, clf4, clf5, clf6, clf7, eclf]
+
+    for name, clf in zip(names, classifiers):
+        prediction_measurements = execute_classifier(clf, name, 5, X, y, prefix_output_filename)
+        #print(name + "'s prediction accuracy (mean of kfolds) is: " + str(prediction_accuracy) + "\n")
+
+        output_row = [name] + list(prediction_measurements)
+        print(output_row)
+
         writer.writerow(output_row)
 
     # close the file
     f.close()
 
-    print("\nComparison done!")
+    print("\n"+input_filename+" comparison done!\n\n")
+
+if __name__ == "__main__":
+    print("Plot classifier comparison started!\n")
+
+    #input_filename = 'nodes_output.csv'
+
+    execute_classifier_comparison("exported_month_large_complete_one_reservoirs_small/1WEEK_nodes_output.csv", "exported_month_large_complete_one_reservoirs_small/1WEEK_")
+    execute_classifier_comparison("exported_month_large_complete_one_reservoirs_small/1MONTH_nodes_output.csv", "exported_month_large_complete_one_reservoirs_small/1MONTH_")
+    execute_classifier_comparison("exported_month_large_complete_one_reservoirs_small/1YEAR_nodes_output.csv", "exported_month_large_complete_one_reservoirs_small/1YEAR_")
+
