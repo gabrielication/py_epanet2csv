e0e828aa813d9f5f65ee47ba7c2732f4a8e50a91
Gabriele Restuccia <gabriele.restuccia92@gmail.com>
2022-06-28 14:19:38 +0200

    new classifier comparison added

--- plot_classifier_comparison.py
+++ plot_classifier_comparison.py
@@ -1,15 +1,12 @@
-from sklearnex import patch_sklearn
-patch_sklearn()
-
 from sklearn.model_selection import KFold
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import confusion_matrix, accuracy_score
 
-from mlxtend.classifier import EnsembleVoteClassifier
 
 import seaborn as sns
 import pandas as pd
@@ -25,7 +22,7 @@ names = [
     "RandomForest",
     "AdaBoostClassifier",
     "GaussianNaiveBayes",
-    "Ensemble",
+    "MLPClassifier",
 ]
 
 def execute_classifier(model, name, k_folds, X, y, prefix_output_filename):
@@ -92,13 +89,13 @@ def execute_classifier(model, name, k_folds, X, y, prefix_output_filename):
     plt.clf()
     return prediction_accuracy, calc_accuracy, precision, recall, false_positive_rate
 
-def execute_classifier_comparison(input_filename, prefix_output_filename):
+def execute_classifier_comparison_wo_smart_sensors(input_filename, prefix_output_filename):
     print("Loading "+input_filename+"...")
 
     data = pd.read_csv(input_filename,
                        names=["hour", "nodeID", "demand_value", "head_value", "pressure_value", "x_pos", "y_pos",
                               "node_type", "has_leak", "leak_area_value", "leak_discharge_value",
-                              "current_leak_demand_value"])
+                              "current_leak_demand_value","smart_sensor_presence"])
 
     output_filename = prefix_output_filename+'prediction_accuracies.csv'
 
@@ -123,10 +120,62 @@ def execute_classifier_comparison(input_filename, prefix_output_filename):
     clf5 = RandomForestClassifier()
     clf6 = AdaBoostClassifier()
     clf7 = GaussianNB()
+    clf8 = MLPClassifier(random_state=1)
 
-    eclf = EnsembleVoteClassifier(clfs=[clf1, clf2, clf3, clf4, clf5, clf6, clf7], weights=[1, 1, 1, 1, 1, 1, 1], voting="hard")
+    #eclf = EnsembleVoteClassifier(clfs=[clf1, clf2, clf3, clf4, clf5, clf6, clf7], weights=[1, 1, 1, 1, 1, 1, 1], voting="hard")
 
-    classifiers = [clf1, clf2, clf3, clf4, clf5, clf6, clf7, eclf]
+    classifiers = [clf1, clf2, clf3, clf4, clf5, clf6, clf7, clf8]
+
+    for name, clf in zip(names, classifiers):
+        prediction_measurements = execute_classifier(clf, name, 5, X, y, prefix_output_filename)
+        #print(name + "'s prediction accuracy (mean of kfolds) is: " + str(prediction_accuracy) + "\n")
+
+        output_row = [name] + list(prediction_measurements)
+        print(output_row)
+
+        writer.writerow(output_row)
+
+    # close the file
+    f.close()
+
+    print("\n"+input_filename+" comparison done!\n\n")
+
+def execute_classifier_comparison_with_smart_sensors(input_filename, prefix_output_filename):
+    print("WITH SENSORS. Loading "+input_filename+"...")
+
+    data = pd.read_csv(input_filename,
+                       names=["hour", "nodeID", "demand_value", "head_value", "pressure_value", "x_pos", "y_pos",
+                              "node_type", "has_leak", "leak_area_value", "leak_discharge_value",
+                              "current_leak_demand_value","smart_sensor_presence"])
+
+    output_filename = prefix_output_filename+'prediction_accuracies.csv'
+
+    # open the file in the write mode
+    f = open(output_filename, 'w')
+
+    # create the csv writer
+    writer = csv.writer(f)
+
+    header = ["Classificator","Accuracy","Calc Accuracy", "Precision", "Recall", "False Positive Rate"]
+    writer.writerow(header)
+
+    print("Dividing X and y matrices...\n")
+
+    X = data[["demand_value", "head_value", "pressure_value","smart_sensor_presence"]].copy()
+    y = data["has_leak"].astype(int)
+
+    clf1 = KNeighborsClassifier()
+    clf2 = SVC(kernel="linear")
+    clf3 = SVC(kernel="rbf",gamma='auto')
+    clf4 = DecisionTreeClassifier()
+    clf5 = RandomForestClassifier()
+    clf6 = AdaBoostClassifier()
+    clf7 = GaussianNB()
+    clf8 = MLPClassifier(random_state=1)
+
+    #eclf = EnsembleVoteClassifier(clfs=[clf1, clf2, clf3, clf4, clf5, clf6, clf7], weights=[1, 1, 1, 1, 1, 1, 1], voting="hard")
+
+    classifiers = [clf1, clf2, clf3, clf4, clf5, clf6, clf7, clf8]
 
     for name, clf in zip(names, classifiers):
         prediction_measurements = execute_classifier(clf, name, 5, X, y, prefix_output_filename)
@@ -147,7 +196,14 @@ if __name__ == "__main__":
 
     #input_filename = 'nodes_output.csv'
 
-    execute_classifier_comparison("exported_month_large_complete_one_reservoirs_small/1WEEK_nodes_output.csv", "exported_month_large_complete_one_reservoirs_small/1WEEK_")
-    execute_classifier_comparison("exported_month_large_complete_one_reservoirs_small/1MONTH_nodes_output.csv", "exported_month_large_complete_one_reservoirs_small/1MONTH_")
-    execute_classifier_comparison("exported_month_large_complete_one_reservoirs_small/1YEAR_nodes_output.csv", "exported_month_large_complete_one_reservoirs_small/1YEAR_")
+    execute_classifier_comparison_wo_smart_sensors("1D_one_res_small/1D_one_res_small_nodes_output.csv", "1D_one_res_small/without_sensors/1D_one_res_small_")
+    execute_classifier_comparison_wo_smart_sensors("1D_one_res_large/1D_one_res_large_nodes_output.csv", "1D_one_res_large/without_sensors/1D_one_res_large_")
+    execute_classifier_comparison_wo_smart_sensors("1D_two_res_large/1D_two_res_large_nodes_output.csv", "1D_two_res_large/without_sensors/1D_two_res_large_")
 
+    #Add sensor field
+    execute_classifier_comparison_with_smart_sensors("1D_one_res_small/1D_one_res_small_nodes_output.csv",
+                                                   "1D_one_res_small/with_sensors/1D_one_res_small_")
+    execute_classifier_comparison_with_smart_sensors("1D_one_res_large/1D_one_res_large_nodes_output.csv",
+                                                   "1D_one_res_large/with_sensors/1D_one_res_large_")
+    execute_classifier_comparison_with_smart_sensors("1D_two_res_large/1D_two_res_large_nodes_output.csv",
+                                                   "1D_two_res_large/with_sensors/1D_two_res_large_")
