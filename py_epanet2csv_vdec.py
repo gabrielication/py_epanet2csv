6177dc8651a468f309e1b629d81c1cfcb02ea062
Gabriele Restuccia <gabriele.restuccia92@gmail.com>
2022-06-28 16:28:22 +0200

    leak percentage fixed, script now outputs sim stats on csv, code cleanup

--- py_epanet2csv_vdec.py
+++ py_epanet2csv_vdec.py
@@ -29,7 +29,7 @@ class WNTR_Process (Process):
         lock.release()
         run_sim(self.wn, self.smart_sensor_junctions, output_file_name=self.name+"_", proc_name=self.name + ": ")
 
-def run_sim(wn, smart_sensor_junctions, output_file_name="", proc_name=""):
+def run_sim(wn, smart_sensor_junctions, number_of_nodes_with_leaks=0, number_of_nodes_with_sensors=0, output_file_name="", proc_name=""):
     print(proc_name + "Configuring simulation...")
 
     wn.options.hydraulic.demand_model = 'PDD' #Pressure Driven Demand mode
@@ -51,21 +51,20 @@ def run_sim(wn, smart_sensor_junctions, output_file_name="", proc_name=""):
 
     print(proc_name + "Simulation finished. Writing to csv (can take a while)...")
 
-    nodes_to_csv(wn, results, node_names, output_file_name, proc_name, smart_sensor_junctions)
+    nodes_to_csv(wn, results, node_names, output_file_name, proc_name, smart_sensor_junctions,number_of_nodes_with_leaks,number_of_nodes_with_sensors)
     links_to_csv(wn, results, link_names, output_file_name, proc_name)
 
     print(proc_name + "Finished!")
 
-def pick_rand_leaks(wn):
+def pick_rand_leaks(wn, number_of_junctions_with_leaks):
     node_names = wn.junction_name_list
 
     #selected_junctions = random.sample(node_names, 3)
 
-    len_nodes = int(len(node_names) / 2)
-    selected_junctions = random.sample(node_names, len_nodes)
+    selected_junctions = random.sample(node_names, number_of_junctions_with_leaks)
 
-    print(len_nodes)
-    print(len(selected_junctions))
+    #print(len_nodes)
+    #print(len(selected_junctions))
 
     return selected_junctions
 
@@ -75,16 +74,15 @@ def assign_leaks(wn, area_size, selected_junctions, proc_name):
 
         node_obj.add_leak(wn, area=area_size, start_time=0)
 
-        print(proc_name + "Leak added to node id: ", node_id)
+        #print(proc_name + "Leak added to node id: ", node_id)
 
-def pick_rand_smart_sensors(wn):
+def pick_rand_smart_sensors(wn,number_of_nodes_with_sensors):
     node_names = wn.junction_name_list
 
-    len_nodes = int(len(node_names) / 4)
-    selected_junctions = random.sample(node_names, len_nodes)
+    selected_junctions = random.sample(node_names, number_of_nodes_with_sensors)
 
-    print(len_nodes)
-    print(len(selected_junctions))
+    #print(len_nodes)
+    #print(len(selected_junctions))
 
     return selected_junctions
 
@@ -140,7 +138,7 @@ def links_to_csv(wn, results, link_names, output_file_name, proc_name):
 
     print(proc_name + "Links' CSV written.")
 
-def nodes_to_csv(wn, results, node_names, output_file_name, proc_name, smart_sensor_junctions):
+def nodes_to_csv(wn, results, node_names, output_file_name, proc_name, smart_sensor_junctions, number_of_nodes_with_leaks, number_of_nodes_with_sensors):
     print(proc_name + "Writing Nodes' CSV...")
 
     demand_results = results.node['demand']
@@ -157,6 +155,10 @@ def nodes_to_csv(wn, results, node_names, output_file_name, proc_name, smart_sen
 
     for timestamp in indexes:
         tot_demand = Decimal('0')
+
+        tot_leak_demand = Decimal('0')
+        tot_nodes_demand = Decimal('0')
+
         demand_value = Decimal('0')
 
         #hour = pd.to_datetime(timestamp, unit='s').time()
@@ -212,7 +214,7 @@ def nodes_to_csv(wn, results, node_names, output_file_name, proc_name, smart_sen
                 print("tot_d")
                 print("{:4.15f}".format(tot_demand))
 
-            tot_demand = tot_demand + demand_value
+            tot_demand = tot_demand + demand_value + current_leak_demand_value
             tot_demand = round(tot_demand,8)
 
             if debug:
@@ -228,6 +230,18 @@ def nodes_to_csv(wn, results, node_names, output_file_name, proc_name, smart_sen
             y_pos = node_obj.coordinates[1]
 
             node_type = node_obj.__class__.__name__
+
+            if(node_type == "Junction"):
+                tot_leak_demand = tot_leak_demand + current_leak_demand_value
+
+                tot_nodes_demand = tot_nodes_demand + demand_value + current_leak_demand_value
+
+                if debug:
+                    print("Node ID: "+str(nodeID))
+                    print("Demand is: "+str(demand_value)+" Leak demand is: "+str(current_leak_demand_value))
+                    print("Tot node demand: "+str(tot_nodes_demand))
+                    print("Tot leak demand: " + str(tot_leak_demand))
+                    print(" ")
 
             output_row = [hour, nodeID, demand_value, head_value, pressure_value, x_pos, y_pos,
                           node_type, has_leak, leak_area_value, leak_discharge_value, current_leak_demand_value, smart_sensor_is_present]
@@ -250,6 +264,47 @@ def nodes_to_csv(wn, results, node_names, output_file_name, proc_name, smart_sen
 
     out.close()
 
+    leak_percentage = (tot_leak_demand/tot_nodes_demand) * 100
+    leak_percentage = round(leak_percentage, 4)
+
+    number_of_nodes = len(wn.node_name_list)
+    number_of_junctions = len(wn.junction_name_list)
+    number_of_reservoirs = len(wn.reservoir_name_list)
+    number_of_tanks = len(wn.tank_name_list)
+    time_spent_on_sim = wn.options.time.duration / 3600
+
+    outName = output_file_name + "nodes_simulation_stats.csv"
+    out = open(outName, "w")
+    writer = csv.writer(out)
+
+    header = ["tot_nodes_demand","leak_percentage","number_of_nodes","number_of_junctions",
+                  "number_of_reservoirs","number_of_tanks","number_of_nodes_with_leaks","number_of_nodes_with_sensors","time_spent_on_sim"]
+
+    writer.writerow(header)
+
+    print("\n\nTot demand for Nodes only is: "+str(tot_nodes_demand)+" and tot_leak_demand is: "+str(tot_leak_demand))
+    print("Total leak demand for nodes is:  "+str(leak_percentage)+"% of the Total Nodes' demand")
+    print("Number of nodes inside of the network is: "+str(number_of_nodes))
+    print("Number of Junctions only: " + str(number_of_junctions))
+    print("Number of Reservoirs only: " + str(number_of_reservoirs))
+    print("Number of Tanks only: " + str(number_of_tanks))
+    print("Number of Junctions with leaks: "+str(number_of_nodes_with_leaks))
+    print("Number of Junctions with sensors: "+str(number_of_nodes_with_sensors))
+    print("Total hours simulated: " + str(time_spent_on_sim)+"\n\n")
+
+    output_row = [tot_nodes_demand,leak_percentage,number_of_nodes,number_of_junctions,
+                  number_of_reservoirs,number_of_tanks,number_of_nodes_with_leaks,number_of_nodes_with_sensors,time_spent_on_sim]
+
+    # print(nodeID)
+    # print(demand_value)
+    # print(tot_demand)
+    # print("Node (" + str(nodeID) + ") demand is: "+str(demand_value)+" so Tot demand is now: " + str(tot_demand))
+    # print("Node ({}) demand is: {} so Tot demand is now: {}".format(nodeID,demand_value,tot_demand))
+
+    writer.writerow(output_row)
+
+    out.close()
+
     print(proc_name + "Nodes' CSV written.")
 
 if __name__ == "__main__":
@@ -258,7 +313,13 @@ if __name__ == "__main__":
     input_file_inp2 = "./networks/exported_month_large_complete_one_reservoirs_large.inp"
     input_file_inp3 = "./networks/exported_month_large_complete_two_reservoirs.inp"
 
-    #Code to be ran with a single execution
+    number_of_junctions_with_leaks1 = 0
+    number_of_junctions_with_leaks2 = 0
+    number_of_junctions_with_leaks3 = 0
+
+    number_of_nodes_with_sensors1 = 0
+    number_of_nodes_with_sensors2 = 0
+    number_of_nodes_with_sensors3 = 0
 
     wn_1 = wntr.network.WaterNetworkModel(input_file_inp1)
     wn_1.options.time.duration = 24 * 3600
@@ -272,30 +333,49 @@ if __name__ == "__main__":
     smart_sensor_junctions = []
 
     if (leaks_enabled):
-        selected_junctions1 = pick_rand_leaks(wn_1)
-        selected_junctions2 = pick_rand_leaks(wn_2)
-        selected_junctions3 = pick_rand_leaks(wn_3)
+        number_of_junctions_with_leaks1 = int(len(wn_1.junction_name_list) / 2)
+        number_of_junctions_with_leaks2 = int(len(wn_2.junction_name_list) / 2)
+        number_of_junctions_with_leaks3 = int(len(wn_3.junction_name_list) / 2)
 
-        assign_leaks(wn_1, 0.05, selected_junctions1, "1D_one_res_small")
-        assign_leaks(wn_2, 0.05, selected_junctions2, "1D_one_res_large")
-        assign_leaks(wn_3, 0.05, selected_junctions3, "1D_two_res_large")
+        selected_junctions1 = pick_rand_leaks(wn_1,number_of_junctions_with_leaks1)
+        selected_junctions2 = pick_rand_leaks(wn_2,number_of_junctions_with_leaks2)
+        selected_junctions3 = pick_rand_leaks(wn_3,number_of_junctions_with_leaks3)
+
+        assign_leaks(wn_1, 0.000002, selected_junctions1, "1D_one_res_small")
+        assign_leaks(wn_2, 0.000006, selected_junctions2, "1D_one_res_large")
+        assign_leaks(wn_3, 0.000006, selected_junctions3, "1D_two_res_large")
 
     if (smart_sensors_enabled):
-        smart_sensor_junctions1 = pick_rand_smart_sensors(wn_1)
-        smart_sensor_junctions2 = pick_rand_smart_sensors(wn_2)
-        smart_sensor_junctions3 = pick_rand_smart_sensors(wn_3)
+        number_of_nodes_with_sensors1 = int(len(wn_1.junction_name_list) / 4)
+        number_of_nodes_with_sensors2 = int(len(wn_2.junction_name_list) / 4)
+        number_of_nodes_with_sensors3 = int(len(wn_3.junction_name_list) / 4)
+
+        smart_sensor_junctions1 = pick_rand_smart_sensors(wn_1,number_of_nodes_with_sensors1)
+        smart_sensor_junctions2 = pick_rand_smart_sensors(wn_2,number_of_nodes_with_sensors2)
+        smart_sensor_junctions3 = pick_rand_smart_sensors(wn_3,number_of_nodes_with_sensors3)
+
+    #Code to be ran with a single execution
+
+    run_sim(wn_1, smart_sensor_junctions1, output_file_name="1D_one_res_small_", proc_name="1D_one_res_small: ",
+            number_of_nodes_with_sensors= number_of_nodes_with_sensors1, number_of_nodes_with_leaks=number_of_junctions_with_leaks1)
+
+    run_sim(wn_2, smart_sensor_junctions2, output_file_name="1D_one_res_large_", proc_name="1D_one_res_large: ",
+            number_of_nodes_with_sensors= number_of_nodes_with_sensors2, number_of_nodes_with_leaks=number_of_junctions_with_leaks2)
+
+    run_sim(wn_3, smart_sensor_junctions3, output_file_name="1D_two_res_large_", proc_name="1D_two_res_large: ",
+            number_of_nodes_with_sensors= number_of_nodes_with_sensors3, number_of_nodes_with_leaks=number_of_junctions_with_leaks3)
 
     #Code to be ran with multiple execution (useful for producing parallel multiple leaks)
-    proc1 = WNTR_Process("1D_one_res_small", wn_1, smart_sensor_junctions1)
-    proc2 = WNTR_Process("1D_one_res_large", wn_2, smart_sensor_junctions2)
-    proc3 = WNTR_Process("1D_two_res_large", wn_3, smart_sensor_junctions3)
+    #proc1 = WNTR_Process("1D_one_res_small", wn_1, smart_sensor_junctions1)
+    #proc2 = WNTR_Process("1D_one_res_large", wn_2, smart_sensor_junctions2)
+    #proc3 = WNTR_Process("1D_two_res_large", wn_3, smart_sensor_junctions3)
 
-    proc1.start()
-    proc2.start()
-    proc3.start()
+    #proc1.start()
+    #proc2.start()
+    #proc3.start()
 
-    proc1.join()
-    proc2.join()
-    proc3.join()
+    #proc1.join()
+    #proc2.join()
+    #proc3.join()
 
     print("Exiting...")
