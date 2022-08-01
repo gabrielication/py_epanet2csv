f37b2b01e503a74af5e46e2971c8151f8b4813be
Domenico Garlisi <domenico.garlisi@dieet.unipa.it>
2022-08-01 14:29:01 +0200

    update ml computing

--- lora_gw_ml_analysis.py
+++ lora_gw_ml_analysis.py
@@ -11,6 +11,7 @@ import csv
 import os
 import pandas as pd
 import numpy as np
+import sys
 
 
 def fit_on_complete_dataset(model, input_full_dataset, writer):
@@ -59,27 +60,50 @@ def execute_classifier(model, input_gw_dataset, gw_id, writer):
     print("Loading " + input_gw_dataset + "...")
 
     data_gw = pd.read_csv(input_gw_dataset)
+    # print(data_gw.head())
+    """
+     hour nodeID  demand_value  ...  current_leak_demand_value     gw_rssi  gw_sf
+    """
+
+    # data_gw = pd.read_csv("1M_one_res_large_nodes_output.csv",
+    #                         names=["hour", "nodeID", "demand_value", "head_value", "pressure_value", "x_pos", "y_pos",
+    #                                "node_type", "has_leak", "leak_area_value", "leak_discharge_value",
+    #                                "current_leak_demand_value", "smart_sensor_presence"])
+    # print(data_gw.head())
 
     # cut dataset to 4 weeks
     end_index_dataset = len(data_gw["nodeID"].unique()) * 24 * 28
+    start_index_test = len(data_gw["nodeID"].unique()) * 24 * 21
+    data_gw = data_gw.iloc[start_index_test:end_index_dataset]
+    data_gw.reset_index(inplace=True, drop=True)
 
-    data_gw = data_gw.iloc[:end_index_dataset]
+    print("Cuting " + input_gw_dataset + "...")
+    # print(data_gw.head())
 
     print("Dividing X_gw and y_gw matrices...")
-
-    X_gw = data_gw[["demand_value", "head_value", "pressure_value"]].copy()
-    y_gw = data_gw["has_leak"].astype(int)
+    X_test_gw = data_gw[["demand_value", "head_value", "pressure_value"]].copy()
+    y_test_gw = data_gw["has_leak"].astype(int)
 
     # test dataset represent only the last week
-
-    start_index_test = len(data_gw["nodeID"].unique()) * 24 * 21
-
-    X_test_gw = X_gw.iloc[start_index_test:]
-    y_test_gw = y_gw.iloc[start_index_test:]
-
     print("Predicting...")
 
     y_pred_test_gw = model.predict(X_test_gw)
+
+    data_gw['y_pred_test_gw'] = y_pred_test_gw
+
+
+    print("len all : ", len(y_pred_test_gw), " - ", len(X_test_gw))
+    print("len all dataset : ", len(data_gw))
+
+    idx  = data_gw.index[data_gw['gw_sf'] == 7].tolist()
+
+    print("len only covered node : ", len(idx))
+
+    y_test_gw = y_test_gw.iloc[idx]
+    y_pred_test_gw = y_pred_test_gw[idx]
+
+    print("len all 2 : ", len(y_pred_test_gw), " - ", len(y_test_gw))
+
 
     produce_results_from_cf_matrix(y_test_gw, y_pred_test_gw, gw_id, writer)
 
@@ -119,7 +143,7 @@ def produce_results_from_cf_matrix(y_test, y_pred, gw_id, writer):
 
     false_positive_rate = false_positives / (false_positives + true_negatives)
 
-    #print(calc_accuracy,precision,recall,fscore,support,false_positive_rate)
+    print(calc_accuracy,precision,recall,fscore,support,false_positive_rate)
 
     out_row = [gw_id, calc_accuracy, precision, recall, fscore, false_positive_rate, true_negatives, false_positives, false_negatives, true_positives]
     writer.writerow(out_row)
@@ -127,6 +151,10 @@ def produce_results_from_cf_matrix(y_test, y_pred, gw_id, writer):
 
 def execute_classifier_for_each_gw(model, input_full_dataset, folder_prefix="", model_out_prefix=""):
     output_filename = model_out_prefix+'lora_all_gw_ml_results_'+input_full_dataset
+
+
+    print("output_filename : ",output_filename)
+    print("input_full_dataset : ", input_full_dataset)
 
     # open the file in the write mode
     f = open(output_filename, "w", newline='', encoding='utf-8')
@@ -141,16 +169,13 @@ def execute_classifier_for_each_gw(model, input_full_dataset, folder_prefix="", 
     model_fitted = fit_on_complete_dataset(model, input_full_dataset, writer)
 
     # we have to iterate each gateway and produce a report from its prediction
-    gw_id = 0
-    index = str(gw_id)
-    input_gw_dataset = folder_prefix+"gw_" + index + "_lora" + input_full_dataset
-
-    while os.path.exists(input_gw_dataset):
-        execute_classifier(model_fitted, input_gw_dataset, gw_id, writer)
-
-        gw_id += 1
+    for gw_id in range(10, 49):
         index = str(gw_id)
-        input_gw_dataset = folder_prefix+"gw_" + index + "_lora" + input_full_dataset
+        input_gw_dataset = folder_prefix + "gw_" + index + "_lora" + input_full_dataset
+        print("\n\n *******input_gw_dataset : ", input_gw_dataset)
+        print(gw_id)
+        if os.path.exists(input_gw_dataset):
+            execute_classifier(model_fitted, input_gw_dataset, gw_id, writer)
 
     f.close()
 
@@ -159,60 +184,60 @@ if __name__ == "__main__":
 
     # DECISION TREE
 
-    input_full_dataset = "1M_one_res_small_nodes_output.csv"
-    folder_prefix = "lora_gw_datasets/"
+    # input_full_dataset = "1M_one_res_small_nodes_output.csv"
+    # folder_prefix = "lora_gw_datasets/"
+    #
+    # model = Pipeline([('scaler', StandardScaler()), ('DTC', DecisionTreeClassifier())])
+    # execute_classifier_for_each_gw(model, input_full_dataset, folder_prefix, "dt_")
 
-    model = Pipeline([('scaler', StandardScaler()), ('DTC', DecisionTreeClassifier())])
-    execute_classifier_for_each_gw(model, input_full_dataset, folder_prefix, "dt_")
+    # input_full_dataset = "1M_one_res_large_nodes_output.csv"
+    # folder_prefix = "lora_gw_datasets/"
+    #
+    # model = Pipeline([('scaler', StandardScaler()), ('DTC', DecisionTreeClassifier())])
+    # execute_classifier_for_each_gw(model, input_full_dataset, folder_prefix, "dt_")
 
+    # input_full_dataset = "1M_two_res_large_nodes_output.csv"
+    # folder_prefix = "lora_gw_datasets/"
+    #
+    # model = Pipeline([('scaler', StandardScaler()), ('DTC', DecisionTreeClassifier())])
+    # execute_classifier_for_each_gw(model, input_full_dataset, folder_prefix, "dt_")
+    #
+    # # MLP
+    #
+    # input_full_dataset = "1M_one_res_small_nodes_output.csv"
+    # folder_prefix = "lora_gw_datasets/"
+    #
+    # model = Pipeline([('scaler', StandardScaler()),
+    #                   ('MLPC', MLPClassifier(random_state=1, max_iter=1000, hidden_layer_sizes=(100)))])
+    # execute_classifier_for_each_gw(model, input_full_dataset, folder_prefix, "mlp_1l_100u_")
+    #
+    # model = Pipeline([('scaler', StandardScaler()),
+    #                   ('MLPC', MLPClassifier(random_state=1, max_iter=1000, hidden_layer_sizes=(100, 100)))])
+    # execute_classifier_for_each_gw(model, input_full_dataset, folder_prefix, "mlp_2l_100u_")
+    #
+    #
     input_full_dataset = "1M_one_res_large_nodes_output.csv"
     folder_prefix = "lora_gw_datasets/"
-
-    model = Pipeline([('scaler', StandardScaler()), ('DTC', DecisionTreeClassifier())])
-    execute_classifier_for_each_gw(model, input_full_dataset, folder_prefix, "dt_")
-
-    input_full_dataset = "1M_two_res_large_nodes_output.csv"
-    folder_prefix = "lora_gw_datasets/"
-
-    model = Pipeline([('scaler', StandardScaler()), ('DTC', DecisionTreeClassifier())])
-    execute_classifier_for_each_gw(model, input_full_dataset, folder_prefix, "dt_")
-
-    # MLP
-
-    input_full_dataset = "1M_one_res_small_nodes_output.csv"
-    folder_prefix = "lora_gw_datasets/"
-
-    model = Pipeline([('scaler', StandardScaler()),
-                      ('MLPC', MLPClassifier(random_state=1, max_iter=1000, hidden_layer_sizes=(100)))])
-    execute_classifier_for_each_gw(model, input_full_dataset, folder_prefix, "mlp_1l_100u_")
-
-    model = Pipeline([('scaler', StandardScaler()),
-                      ('MLPC', MLPClassifier(random_state=1, max_iter=1000, hidden_layer_sizes=(100, 100)))])
-    execute_classifier_for_each_gw(model, input_full_dataset, folder_prefix, "mlp_2l_100u_")
-
-
-    input_full_dataset = "1M_one_res_large_nodes_output.csv"
-    folder_prefix = "lora_gw_datasets/"
-
+    #
     model = Pipeline([('scaler', StandardScaler()),
                       ('MLPC', MLPClassifier(random_state=1, max_iter=1000, hidden_layer_sizes=(2000)))])
     execute_classifier_for_each_gw(model, input_full_dataset, folder_prefix, "mlp_1l_2000u_")
-
-    model = Pipeline([('scaler', StandardScaler()),
-                      ('MLPC', MLPClassifier(random_state=1, max_iter=1000, hidden_layer_sizes=(2000, 2000)))])
-    execute_classifier_for_each_gw(model, input_full_dataset, folder_prefix, "mlp_2l_2000u_")
-
-
-    input_full_dataset = "1M_two_res_large_nodes_output.csv"
-    folder_prefix = "lora_gw_datasets/"
-
-    model = Pipeline([('scaler', StandardScaler()),
-                      ('MLPC', MLPClassifier(random_state=1, max_iter=1000, hidden_layer_sizes=(4000)))])
-    execute_classifier_for_each_gw(model, input_full_dataset, folder_prefix, "mlp_1l_4000u_")
-
-    model = Pipeline([('scaler', StandardScaler()),
-                      ('MLPC', MLPClassifier(random_state=1, max_iter=1000, hidden_layer_sizes=(4000, 4000)))])
-    execute_classifier_for_each_gw(model, input_full_dataset, folder_prefix, "mlp_2l_4000u_")
+    #
+    # model = Pipeline([('scaler', StandardScaler()),
+    #                   ('MLPC', MLPClassifier(random_state=1, max_iter=1000, hidden_layer_sizes=(2000, 2000)))])
+    # execute_classifier_for_each_gw(model, input_full_dataset, folder_prefix, "mlp_2l_2000u_")
+    #
+    #
+    # input_full_dataset = "1M_two_res_large_nodes_output.csv"
+    # folder_prefix = "lora_gw_datasets/"
+    #
+    # model = Pipeline([('scaler', StandardScaler()),
+    #                   ('MLPC', MLPClassifier(random_state=1, max_iter=1000, hidden_layer_sizes=(4000)))])
+    # execute_classifier_for_each_gw(model, input_full_dataset, folder_prefix, "mlp_1l_4000u_")
+    #
+    # model = Pipeline([('scaler', StandardScaler()),
+    #                   ('MLPC', MLPClassifier(random_state=1, max_iter=1000, hidden_layer_sizes=(4000, 4000)))])
+    # execute_classifier_for_each_gw(model, input_full_dataset, folder_prefix, "mlp_2l_4000u_")
 
 
     print("\nDecision Tree benchmark done!\n")
