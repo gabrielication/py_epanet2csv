d48517bc0763069e207262f84488c21901cd883e
Gabriele Restuccia <gabriele.restuccia92@gmail.com>
2022-12-14 17:31:06 +0100

    new function to plot data, csvs added

--- plot_tensorflow_results.py
+++ plot_tensorflow_results.py
@@ -6,9 +6,7 @@ from itertools import combinations
 # https://stackabuse.com/change-tick-frequency-in-matplotlib/
 # https://sparkbyexamples.com/pandas/pandas-select-dataframe-rows-based-on-column-values/
 
-
-
-def save_delta_plot_after_tensorflow_analysis(path, bool_base_demand, bool_pressure_value, y_data, save=True, show=False):
+def delta_plot_after_tensorflow_analysis(path, bool_base_demand, bool_pressure_value, y_data, save=True, show=False):
     data = pd.read_csv(path)
 
     data = data.loc[(data['base_demand'] == bool_base_demand) & (data['pressure_value'] == bool_pressure_value)]
@@ -34,8 +32,6 @@ def save_delta_plot_after_tensorflow_analysis(path, bool_base_demand, bool_press
 
     output_filename = output_filename+".png"
 
-    print(output_filename)
-
     # plt.yticks(np.arange(0, max(y), 0.00001))
 
     # fig = plt.figure()
@@ -59,6 +55,40 @@ def save_delta_plot_after_tensorflow_analysis(path, bool_base_demand, bool_press
 
     if(save):
         plt.savefig(output_filename, dpi=300, bbox_inches = "tight")
+        print("Saved to: " + output_filename)
+
+    if(show):
+        plt.show()
+
+    plt.clf()
+
+
+def simple_features_combination_plot_after_tensorflow_analysis(path, dataset, y_data, save=True, show=False):
+    data = pd.read_csv(path)
+
+    data = data.loc[(data['dataset'] == dataset)]
+
+    x = ["base_demand","pressure","bd+press"]
+    y = data[y_data]
+
+    time_period = path[0:2]
+
+    plot_title = time_period+" "+y_data+" with feat combinations"
+
+    output_filename = time_period+"_"+y_data+"_with_feat_combinations"
+    output_filename = output_filename+".png"
+
+    plt.plot(x, y)
+    plt.title(plot_title)
+
+    plt.xlabel("features")
+    plt.ylabel(y_data)
+
+    plt.grid(True)
+
+    if(save):
+        plt.savefig(output_filename, dpi=300, bbox_inches = "tight")
+        print("Saved to: "+output_filename)
 
     if(show):
         plt.show()
@@ -79,15 +109,26 @@ if __name__ == "__main__":
     bool_base_demand = True
     bool_pressure_value = False
 
-    save_delta_plot_after_tensorflow_analysis(path, bool_base_demand, bool_pressure_value, y_data, save=save, show=show)
+    delta_plot_after_tensorflow_analysis(path, bool_base_demand, bool_pressure_value, y_data, save=save, show=show)
 
     bool_base_demand = False
     bool_pressure_value = True
 
-    save_delta_plot_after_tensorflow_analysis(path, bool_base_demand, bool_pressure_value, y_data, save=save, show=show)
+    delta_plot_after_tensorflow_analysis(path, bool_base_demand, bool_pressure_value, y_data, save=save, show=show)
 
     bool_base_demand = True
     bool_pressure_value = True
 
-    save_delta_plot_after_tensorflow_analysis(path, bool_base_demand, bool_pressure_value, y_data, save=save, show=show)
+    delta_plot_after_tensorflow_analysis(path, bool_base_demand, bool_pressure_value, y_data, save=save, show=show)
 
+    #################
+
+    dataset = "1W_one_res_small_no_leaks_rand_base_dem_nodes_output.csv"
+
+    y_data = 'epochs'
+
+    simple_features_combination_plot_after_tensorflow_analysis(path, dataset, y_data, save=save, show=show)
+
+    y_data = 'loss'
+
+    simple_features_combination_plot_after_tensorflow_analysis(path, dataset, y_data, save=save, show=show)
