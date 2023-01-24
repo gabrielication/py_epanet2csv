09688ab372bbe39e8bbea8b8481ed8cb620228f7
Domenico Garlisi <domenico.garlisi@dieet.unipa.it>
2023-01-24 08:40:35 +0100

    add dataset for plot

--- plot_tensorflow_results.py
+++ plot_tensorflow_results.py
@@ -113,57 +113,79 @@ def plot_user_demand(path, save=False, show=True):
     # np.random.seed(19680801)
 
     fig = plt.figure()
-    ax = fig.add_subplot(projection='3d')
 
-    data = pd.read_csv(path)
+    for ii in range(1,3):
 
-    colors = ['r', 'g', 'b', 'y', 'c', 'm', 'k', 'w']
-    # yticks = [7, 6, 5, 4, 3, 2, 1, 0]
-    yticks = [0,1,2,3,4,5,6,7]
+        ax1 = fig.add_subplot(1, 2, ii, projection='3d')
+        # create two subplots with the shared x and y axes
+        # fig, (ax1, ax2) = plt.subplots(1, 2, projection='3d', sharex=False, sharey=False)
 
-    nodeList = data.nodeID.unique()
-    nodeIndex = 0
-    for node in nodeList:
-        dataToPlot = data.loc[(data['nodeID'] == node)]
-        #hour,nodeID,base_demand,demand_value,head_value,pressure_value,x_pos,y_pos,node_type,has_leak,leak_area_value,leak_discharge_value,current_leak_demand_value,smart_sensor_is_present,tot_network_demand
-        demandToPlot = dataToPlot['demand_value'].values
-        baseDemandToPlot = dataToPlot['base_demand'].values
+        data = pd.read_csv(path)
 
-        # for c, k in zip(colors, yticks):
-        c = colors[nodeIndex]; k = yticks[nodeIndex];
+        colors = ['r', 'g', 'b', 'y', 'c', 'm', 'k', 'w']
+        # yticks = [7, 6, 5, 4, 3, 2, 1, 0]
+        yticks = [0,1,2,3,4,5,6,7]
 
-        # create data for the y=k 'layer'.
-        xs = np.arange(len(demandToPlot))
-        # ys = demandToPlot
-        ys = baseDemandToPlot
+        nodeList = data.nodeID.unique()
+        print(nodeList)
 
-        # You can provide either a single color or an array with the same length as
-        # xs and ys. To demonstrate this, we color the first bar of each set cyan.
-        cs = [c] * len(xs)
-        # cs[0] = 'c'
+        nodeIndex = 0
+        nodeSelected = []
+        for iterationIndex in range(4):
+            dataToPlot = data.loc[(data['nodeID'] == nodeList[nodeIndex])]
+            nodeSelected.append(nodeList[nodeIndex])
 
-        # Plot the bar graph given by xs and ys on the plane y=k with 80% opacity.
-        ax.bar(xs, ys, k, zdir='y', color=cs, alpha=0.8)
+            #hour,nodeID,base_demand,demand_value,head_value,pressure_value,x_pos,y_pos,node_type,has_leak,leak_area_value,leak_discharge_value,current_leak_demand_value,smart_sensor_is_present,tot_network_demand
+            baseDemandToPlot = dataToPlot['base_demand'].values #*  3.785412 * 60
+            demandToPlot = dataToPlot['demand_value'].values #*  3.785412 * 60
 
-        # ls = LightSource(270, 45)
-        # # To use a custom hillshading mode, override the built-in shading and pass
-        # # in the rgb colors of the shaded surface calculated from "shade".
-        # rgb = ls.shade(z, cmap=cm.gist_earth, vert_exag=0.1, blend_mode='soft')
-        # surf = ax.plot_surface(x, y, z, rstride=1, cstride=1, facecolors=rgb,
-        #                        linewidth=0, antialiased=False, shade=False)
+            # for c, k in zip(colors, yticks):
+            c = colors[iterationIndex]; k = yticks[iterationIndex];
 
-        nodeIndex += 1
-        # if nodeIndex>len(colors)-1:
-        if nodeIndex > 3:
-            break
+            # create data for the y=k 'layer'.
+            xs = np.arange(len(demandToPlot))
+            # ys = demandToPlot
 
-    ax.set_xlabel('Hours')
-    ax.set_ylabel('Node')
-    ax.set_zlabel('Demand')
+            if ii == 1:
+                ys = baseDemandToPlot
+            else:
+                ys = demandToPlot
 
-    # On the y axis let's only label the discrete values that we have data for.
-    # ax.set_yticks(yticks)
-    ax.set_yticks([1,2,3,4])
+            # You can provide either a single color or an array with the same length as
+            # xs and ys. To demonstrate this, we color the first bar of each set cyan.
+            cs = [c] * len(xs)
+            # cs[0] = 'c'
+
+            # Plot the bar graph given by xs and ys on the plane y=k with 80% opacity.
+            ax1.bar(xs, ys, k, zdir='y', color=cs, alpha=0.8)
+
+            # ls = LightSource(270, 45)
+            # # To use a custom hillshading mode, override the built-in shading and pass
+            # # in the rgb colors of the shaded surface calculated from "shade".
+            # rgb = ls.shade(z, cmap=cm.gist_earth, vert_exag=0.1, blend_mode='soft')
+            # surf = ax.plot_surface(x, y, z, rstride=1, cstride=1, facecolors=rgb,
+            #                        linewidth=0, antialiased=False, shade=False)
+
+            nodeIndex += 15
+            # if nodeIndex>len(colors)-1:
+            # if nodeIndex > 3:
+            #     break
+
+        ax1.set_xlabel('Hours')
+        ax1.set_ylabel('Node')
+        if ii == 1:
+            ax1.set_zlabel('Base demand [G/M]')
+            # ax1.set_zlabel('Base demand [l/H]')
+        else:
+            ax1.set_zlabel('Demand value [G/M]')
+            # ax1.set_zlabel('Demand value [l/H]')
+
+        # On the y axis let's only label the discrete values that we have data for.
+        # ax.set_yticks(yticks)
+        ax1.set_yticks([1,2,3,4])
+        # ax1.set_ylabel(nodeSelected)
+        # ax1.set_yticklabels(ax1.get_yticks(), rotation=30)
+        ax1.set_yticklabels(nodeSelected, rotation=90)
 
     # bool_base_demand = True
     # bool_pressure_value = False
@@ -220,6 +242,165 @@ def plot_user_demand(path, save=False, show=True):
         plt.show()
 
     plt.clf()
+
+
+
+def plot_user_demand_v2(path, save=False, show=True):
+
+    # Fixing random state for reproducibility
+    # np.random.seed(19680801)
+
+
+
+    # create two subplots with the shared x and y axes
+    # fig, (ax1, ax2) = plt.subplots(1, 2, projection='3d', sharex=False, sharey=False)
+
+    data = pd.read_csv(path)
+
+    colors = ['r', 'g', 'b', 'y', 'c', 'm', 'k', 'w']
+    # yticks = [7, 6, 5, 4, 3, 2, 1, 0]
+    yticks = [0,1,2,3,4,5,6,7]
+
+    nodeList = data.nodeID.unique()
+    print(nodeList)
+
+    for ii in range(0,4):
+        print("FIGURE : ", ii)
+        # fig = plt.figure()
+
+        if ii<3:
+            fig, axs = plt.subplots(4, 6, sharex=True, sharey=True, gridspec_kw={'hspace': 0})
+        else:
+            fig, axs = plt.subplots(2, 6, sharex=True, sharey=True, gridspec_kw={'hspace': 0})
+
+        # fig.suptitle('Sharing both axes')
+
+        nodeIndex = 1
+        plotIndexRow = 0
+        plotIndexColumn = 0
+        for node in nodeList[ii*24:(ii+1)*24]:
+            print("SUBPLOT node : ", node)
+            # ax1 = fig.add_subplot(4, 6, nodeIndex)
+
+            dataToPlot = data.loc[(data['nodeID'] == node)]
+
+            #hour,nodeID,base_demand,demand_value,head_value,pressure_value,x_pos,y_pos,node_type,has_leak,leak_area_value,leak_discharge_value,current_leak_demand_value,smart_sensor_is_present,tot_network_demand
+            baseDemandToPlot = dataToPlot['base_demand'].values #*  3.785412 * 60
+            #demandToPlot = dataToPlot['demand_value'].values #*  3.785412 * 60
+
+            # for c, k in zip(colors, yticks):
+            # c = colors[nodeIndex]; k = yticks[nodeIndex];
+
+            # create data for the y=k 'layer'.
+            xs = np.arange(len(baseDemandToPlot))
+
+            # You can provide either a single color or an array with the same length as
+            # xs and ys. To demonstrate this, we color the first bar of each set cyan.
+            # cs = [c] * len(xs)
+            # cs[0] = 'c'
+
+            # Plot the bar graph given by xs and ys on the plane y=k with 80% opacity.
+            # ax1.bar(xs, ys, k, zdir='y', color=cs, alpha=0.8)
+            # ax1.plot(xs, baseDemandToPlot, label='Base Demand node ' + str(node))
+            # ax1.plot(xs, demandToPlot, label='Demand value node ' + str(node))
+
+            # axs[plotIndexRow, plotIndexColumn].plot(xs, baseDemandToPlot, label='Base Demand node ' + str(node))
+            # axs[plotIndexRow, plotIndexColumn].plot(xs, demandToPlot, label='Demand value node ' + str(node))
+            axs[plotIndexRow, plotIndexColumn].plot(xs, baseDemandToPlot, label='Base Demand node')
+            # axs[plotIndexRow, plotIndexColumn].plot(xs, demandToPlot, label='Demand value node ')
+
+            # ls = LightSource(270, 45)
+            # # To use a custom hillshading mode, override the built-in shading and pass
+            # # in the rgb colors of the shaded surface calculated from "shade".
+            # rgb = ls.shade(z, cmap=cm.gist_earth, vert_exag=0.1, blend_mode='soft')
+            # surf = ax.plot_surface(x, y, z, rstride=1, cstride=1, facecolors=rgb,
+            #                        linewidth=0, antialiased=False, shade=False)
+
+            axs[plotIndexRow, plotIndexColumn].set_xlabel('Hours')
+            if plotIndexColumn == 0:
+                axs[plotIndexRow, plotIndexColumn].set_ylabel('[G/M]')
+
+            plotIndexColumn += 1
+            if plotIndexColumn > 5:
+                plotIndexRow += 1
+                plotIndexColumn = 0
+
+
+            nodeIndex += 1
+            # if nodeIndex>len(colors)-1:
+            if nodeIndex > 24:
+                break
+
+        axs[0, 5].legend()
+
+        # if ii == 1:
+        #     ax1.set_zlabel('Base demand [G/M]')
+        # else:
+        #     ax1.set_zlabel('Demand value [G/M]')
+
+        # On the y axis let's only label the discrete values that we have data for.
+        # ax.set_yticks(yticks)
+        # ax1.set_yticks([1,2,3,4])
+
+        # bool_base_demand = True
+        # bool_pressure_value = False
+        # data = data.loc[(data['base_demand'] == bool_base_demand) & (data['pressure_value'] == bool_pressure_value)]
+        #
+        # # x = ["no leak", "1 leak", "1/8 leaks", "1/4 leaks", "1/2 leaks"]
+        # # y = data[y_data]
+        #
+        # time_period = path[0:2]
+        #
+        # plot_title = time_period + " with "
+
+        # output_filename = "one_res_small_no_leaks_node_demand_base_value_" + str(ii) + "_" #+ y_data + "_with_"
+        output_filename = path.replace("/", "-") + str(ii)
+
+        # # output_filename = "1W_delta_loss_with_bdem_press.png"
+        #
+        # if (bool_base_demand == True and bool_pressure_value == False):
+        #     plot_title = plot_title + "base_demand"
+        #     output_filename = output_filename + "base_demand"
+        # elif (bool_base_demand == False and bool_pressure_value == True):
+        #     plot_title = plot_title + "pressure"
+        #     output_filename = output_filename + "pressure"
+        # elif (bool_base_demand == True and bool_pressure_value == True):
+        #     plot_title = plot_title + "base_demand and pressure"
+        #     output_filename = output_filename + "base_demand_pressure"
+        #
+        output_filename = "fig/" + output_filename + ".png"
+        #
+        # # plt.yticks(np.arange(0, max(y), 0.00001))
+        #
+        # # fig = plt.figure()
+        # # ax = fig.add_subplot()
+        #
+        # # plt.plot(x, y)
+        # plt.title(plot_title)
+        #
+        # # plt.plot(x,y,'ro')
+        #
+        # # for i,j in zip(x,y):
+        # #     ax.annotate(str(j), xy=(i,j), xytext=(10,20), textcoords='offset points')
+        #
+        # plt.xlabel('Dataset')
+        # # plt.ylabel('Delta Loss')
+        #
+        # # plt.ylabel(y_data)
+        # plt.ylabel("user demand")
+        #
+        # # plt.legend()
+        # plt.grid(True)
+        #
+        if (save):
+            plt.savefig(output_filename, dpi=300, bbox_inches="tight")
+            print("Saved to: " + output_filename)
+
+        if (show):
+            plt.show()
+
+        plt.clf()
+
 
 def plot_hist_user_demand(path, save=False, show=True):
 
@@ -374,16 +555,20 @@ def plot_cumulative_hist_user_demand(path, save=False, show=True):
     nodeList = data.nodeID.unique()
     nodeIndex = 0
     for node in nodeList:
+        print(node)
         dataToPlot = data.loc[(data['nodeID'] == node)]
         #hour,nodeID,base_demand,demand_value,head_value,pressure_value,x_pos,y_pos,node_type,has_leak,leak_area_value,leak_discharge_value,current_leak_demand_value,smart_sensor_is_present,tot_network_demand
-        demandToPlot = dataToPlot['demand_value'].values
+
+        # demandToPlot = dataToPlot['demand_value'].values
         baseDemandToPlot = dataToPlot['base_demand'].values
 
-        # count, division = np.histogram(baseDemandToPlot, bins=40, range=[0, 0.01], density=False)
+        count, division = np.histogram(baseDemandToPlot, bins=40, range=[0, 0.01], density=False)
         # count, division = np.histogram(baseDemandToPlot, bins=20, density=False)
-        count, division = np.histogram(baseDemandToPlot, bins=20)
-        # print(count)
-        # print(division)
+        #count, division = np.histogram(baseDemandToPlot, bins=20)
+
+
+        print(count)
+        print(division)
 
         width = 0.7 * (division[1] - division[0])
         center = (division[:-1] + division[1:]) / 2
@@ -405,7 +590,7 @@ def plot_cumulative_hist_user_demand(path, save=False, show=True):
 
         nodeIndex += 1
         # if nodeIndex>len(colors)-1:
-        if nodeIndex > 81:
+        if nodeIndex > 3: #81:
             break
 
     # ax.set_xticklabels(center.round(6), rotation=65)
@@ -519,7 +704,14 @@ if __name__ == "__main__":
     - nel secondo tipo di grafico vogliamo plottare l'errore, cambiando il plot facendo vedere una superficie: https://matplotlib.org/stable/gallery/mplot3d/custom_shaded_3d_surface.html#sphx-glr-gallery-mplot3d-custom-shaded-3d-surface-py
     - SECONDO TIPO DI GRAFICO: 9 nodi per ogni grafico, tempo sulle x, nodo sul lambda, valore delta delle predictions-valore vero su 'probability'
     """
-    path = "tensorflow_datasets/1W_one_res_small_no_leaks_rand_base_dem_nodes_output.csv"
-    plot_user_demand(path, save=True, show=False)
-    plot_hist_user_demand(path, save=True, show=False)
-    plot_cumulative_hist_user_demand(path, save=True, show=False)
+    path_rand_demand = "tensorflow_datasets/one_res_small/no_leaks_rand_base_demand/1W/1W_one_res_small_no_leaks_rand_base_dem_nodes_output.csv"
+    # # plot_user_demand(path_rand_demand, save=False, show=True)
+    # plot_user_demand_v2(path_rand_demand, save=False, show=True)
+    # # plot_hist_user_demand(path_rand_demand, save=True, show=False)
+    # # plot_cumulative_hist_user_demand(path_rand_demand, save=True, show=False)
+
+    path_pattern_demand = "tensorflow_datasets/one_res_small/no_leaks_pattern_demand/1M/1M_one_res_large_nodes_output.csv"
+    # plot_user_demand(path_pattern_demand, save=False, show=True)
+    #plot_user_demand_v2(path_pattern_demand, save=True, show=False)
+    # # plot_hist_user_demand(path_pattern_demand, save=True, show=False)
+    plot_cumulative_hist_user_demand(path_pattern_demand, save=False, show=True)
