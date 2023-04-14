60a7b99e9e7fb19f82d39b82cc5a60529efc5f4f
Gabriele Restuccia <gabriele.restuccia92@gmail.com>
2023-04-14 15:23:50 +0200

    code improvement for classifier

--- tensorflow_classifier.py
+++ tensorflow_classifier.py
@@ -1,50 +1,190 @@
+import numpy as np
+import pandas as pd
+import os
+import shutil
+from datetime import datetime
+from pathlib import Path
+
 import tensorflow as tf
+import tensorflow_addons as tfa
+
 from tensorflow.keras.layers import Dense, Flatten
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import BatchNormalization
-import numpy as np
-import pandas as pd
-import tensorflow_addons as tfa
 
-df = pd.read_pickle("tensorflow_datasets/one_res_small/gabriele_marzo_2023/processed_df.pickle")
+# from keras_visualizer import visualizer
 
-target = df.pop("has_leak")
+def is_gpu_supported():
+    gpu_list = tf.config.list_physical_devices('GPU')
 
-data = np.array(df.values.tolist())
+    if (len(gpu_list) == 0):
+        print("GPU IS NOT SUPPORTED/ACTIVE/DETECTED!")
 
-num_samples = data.shape[0]
-num_features = data.shape[1]
-num_channels = len(data[0][0])  # since each feature has 6 floats
+        return False
+    else:
+        print("GPU SUPPORTED: ", gpu_list)
 
-# Reshape the input data into a 3D tensor
-X = np.reshape(data, (num_samples, num_features, num_channels))
+        return True
 
-temp_lab = target.values.tolist()
+def clean_old_models(model_path):
+    print("FRESH START ENABLED. Cleaning ALL old models and their files...")
 
-y = np.reshape(temp_lab, (num_samples, num_features))
+    for filename in Path(".").glob(model_path):
+        try:
+            shutil.rmtree(filename)
 
-# Define the MLP model
-model = Sequential([
-    Flatten(input_shape=(num_features, num_channels)),
-    BatchNormalization(),
-    Dense(1024, activation='relu'),
-    Dense(1024, activation='relu'),
-    Dense(1024, activation='relu'),
-    Dense(num_features, activation='sigmoid')
-])
+            print(str(filename) + " deleted")
+        except OSError:
+            print("\nError while deleting " + str(filename) + "\n")
 
-loss=tf.keras.losses.BinaryCrossentropy()
+    print("All old files deleted.\n")
 
-# accuracy measures the overall correctness of the model's predictions,
-# precision measures how often the model is correct when it predicts a positive instance,
-# recall measures how well the model can identify positive instances,
-# F1 score combines precision and recall to give a single measure of the model's performance
+def load_model(model_path_filename, history_path_filename):
+    if os.path.exists(model_path_filename):
+        print("Model already exists!\nIf tensorflow versions from saved one differ then a crash might happen!")
+        model = tf.keras.models.load_model(model_path_filename)
 
-# These are the metrics for a binary classification problem
-metrics = ['accuracy', tf.keras.metrics.Precision(), tf.keras.metrics.Recall(), tfa.metrics.F1Score(num_classes=83)]
+        history_complete_path = model_path_filename + '/' + history_path_filename + '.npy'
 
-# Compile the model with binary crossentropy loss and Adam optimizer
-model.compile(loss=loss, optimizer='adam', metrics=metrics)
+        history = np.load(history_complete_path, allow_pickle='TRUE').item()
 
-# Train the model
-model.fit(X, y, epochs=100, batch_size=4, validation_split=0.2, shuffle=False)
+        print("Loading model: " + model_path_filename + "/\nLoading history: " + history_complete_path)
+
+        return model, history
+    else:
+        print("Previous fitted model not found!")
+
+        return None, None
+
+def save_model(model, history, model_path_filename, history_path_filename):
+    model.save(model_path_filename)
+
+    history_complete_path = model_path_filename + '/' + history_path_filename + '.npy'
+
+    np.save(history_complete_path, history.history)
+
+    print("Model saved to: " + model_path_filename)
+    print("Training History saved to: " + history_complete_path)
+
+def formatted_datetime():
+    # current date and time
+    now = str(datetime.now())
+    now = now.replace(" ", "_")
+    now = now.replace(".", "_")
+    now = now.replace(":", "_")
+
+    return now
+
+def nn_classifier(folder_path, filename, epochs, batch_size=None,
+                  model_path_filename="", history_path_filename="",
+                  validation_split=0.2, patience_early_stop=10, save_model_bool=False):
+    print("\nNN Classifier launched!\n")
+
+    print("Parameters:")
+    print("epochs:", epochs)
+    print("batch_size:", batch_size)
+    print("model_path_filename:", model_path_filename)
+    print("history_path_filename:", history_path_filename)
+    print("validation_split:", validation_split)
+    print("save_model_bool:", save_model_bool)
+    print("patience_early_stop:", patience_early_stop)
+    print()
+
+    complete_path_filename = folder_path + filename
+
+    df = pd.read_pickle(complete_path_filename)
+
+    target = df.pop("has_leak")
+
+    train_data = np.array(df.values.tolist())
+
+    num_samples = train_data.shape[0]
+    num_features = train_data.shape[1]
+    num_channels = len(train_data[0][0])  # since each feature has 6 floats
+
+    # Reshape the input data into a 3D tensor
+    X = np.reshape(train_data, (num_samples, num_features, num_channels))
+
+    temp_lab = target.values.tolist()
+
+    y = np.reshape(temp_lab, (num_samples, num_features))
+
+    # Define the MLP model
+    model = Sequential([
+        Flatten(input_shape=(num_features, num_channels)),
+        BatchNormalization(),
+        Dense(1024, activation='relu'),
+        Dense(1024, activation='relu'),
+        Dense(1024, activation='relu'),
+        Dense(num_features, activation='sigmoid')
+    ])
+
+    loss = tf.keras.losses.BinaryCrossentropy()
+
+    # accuracy measures the overall correctness of the model's predictions,
+    # precision measures how often the model is correct when it predicts a positive instance,
+    # recall measures how well the model can identify positive instances,
+    # F1 score combines precision and recall to give a single measure of the model's performance
+
+    # These are the metrics for a binary classification problem
+    metrics = ['accuracy', tf.keras.metrics.Precision(), tf.keras.metrics.Recall(), tfa.metrics.F1Score(num_classes=83)]
+
+    # Compile the model with binary crossentropy loss and Adam optimizer
+    model.compile(loss=loss, optimizer='adam', metrics=metrics)
+
+    earlystop = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=patience_early_stop)
+    callbacks = [earlystop]
+
+    # Train the model
+    history = model.fit(X, y, epochs=epochs, batch_size=batch_size, validation_split=validation_split, shuffle=False,
+                        callbacks=callbacks)
+
+    if (save_model_bool):
+        print()
+
+        now = formatted_datetime()
+        save_model(model, history, model_path_filename + "_" + now, history_path_filename)
+
+    # Evaluate the model on the test data using `evaluate`
+    # print("Evaluate on test data")
+    # results = model.evaluate(x_test, y_test, batch_size=128)
+    # print("test loss, test acc:", results)
+
+    return model, history
+
+
+if __name__ == "__main__":
+    print('Tensorflow ', tf.__version__)
+    print('Keras ', tf.keras.__version__)
+    is_gpu_supported()
+
+    folder_path = "tensorflow_datasets/one_res_small/gabriele_marzo_2023/"
+    # filename = "12M_processed_df.pickle"
+    filename = "processed_df.pickle"
+
+    # Where to save/load the fitted model and its history file
+    model_path_filename = "tensorflow_models/classification_12M_processed"
+    history_path_filename = "classification_history_model"
+
+    # This bool will determine if the (new) fitted model will be saved to the path and names indicated above
+    save_model_bool = True
+
+    # epochs during fit
+    epochs = 2
+
+    # batch size to be used during fit
+    batch_size = 4
+
+    # This float will split the data for validation during fit
+    validation_split = 0.2
+
+    # This bool will add Dropouts layers to the NN
+    dropout = True
+
+    # This int determines how many epochs should we monitor before stopping fitting if the situation does not improve
+    patience_early_stop = 10
+
+    nn_classifier(folder_path, filename, epochs, batch_size=batch_size,
+                  model_path_filename=model_path_filename, history_path_filename=history_path_filename,
+                  validation_split=validation_split, patience_early_stop=patience_early_stop,
+                  save_model_bool=save_model_bool)
